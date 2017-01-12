8b4d2e8807af4346c4e0cdf7e47b7796aed142af
Pavel Pravosud <pavel@pravosud.com>
2017-01-11 21:53:27 -0800

    Silence forwardable warnings in ruby 2.4

--- lib/l2meter/null_object.rb
+++ lib/l2meter/null_object.rb
@@ -1,5 +1,10 @@
 module L2meter
   class NullObject
+    # Silence forwardable stdlib warnings about forwarding private metods.
+    def log(*)
+      super
+    end
+
     def method_missing(*)
       yield if block_given?
     end
